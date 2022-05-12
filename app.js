6be17da6cdfb40857c3dfc9a56b0fb51fe53e8f7
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-05-12 14:38:09 -0700

    stylied by css with japanese calendar

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const bodyParser = require("body-parser");
 
 const app = express()
 
-var items = ["Study ejs"];
+var items = ["Study ejs","Workout"];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
 	var today = new Date();
