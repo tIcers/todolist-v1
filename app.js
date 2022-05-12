80b46303e78e5936584d4ccf3ad5bd22135c28fa
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-05-12 13:49:45 -0700

    implemented code to let user add thier items

--- app.js
+++ app.js
@@ -1,42 +1,55 @@
 const express = require('express');
-const { sendFile } = require('express/lib/response');
-const app = express()
 const port = 4000
+const bodyParser = require("body-parser");
+
+const app = express()
+
+var items = ["Study ejs"];
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
 	var today = new Date();
-	var currentDay = today.getDay();
-	var day = " "
 
-	switch (currentDay) {
-		case 0:
-			day = "Sunday";
-			break;
-		case 1:
-			day = "Monday";
-			break;
-		case 2:
-			day = "Thuesday";
-			break;
-		case 3:
-			day = "Wednesday";
-			break;
-		case 4:
-			day = "Thursday";
-			break;
-		case 5:
-			day = "Friday";
-			break;
-		case 6:
-			day = "Saturday";
-			break;
-		default:
-			console.log("Error: current day is qual to :" + currentDay);
+	var options = {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric'
+	};
 
-	}
+	var day = today.toLocaleDateString("ja-JP", options);
+
+
+
+	// switch (currentDay) {
+	// 	case 0:
+	// 		day = "Sunday";
+	// 		break;
+	// 	case 1:
+	// 		day = "Monday";
+	// 		break;
+	// 	case 2:
+	// 		day = "Thuesday";
+	// 		break;
+	// 	case 3:
+	// 		day = "Wednesday";
+	// 		break;
+	// 	case 4:
+	// 		day = "Thursday";
+	// 		break;
+	// 	case 5:
+	// 		day = "Friday";
+	// 		break;
+	// 	case 6:
+	// 		day = "Saturday";
+	// 		break;
+	// 	default:
+	// 		console.log("Error: current day is qual to :" + currentDay);
+
+
 	// if (currentDay === 6 || currentDay === 0) {
 	// 	day = "Weekend";
 	// 	//res.render('list', {kindOfDay:day});
@@ -47,9 +60,17 @@ app.get('/', (req, res) => {
 	//res.write("<p>Its not a weekend, i have to work on assignment!</p>");
 	//res.write("<h1>No, i have to work! IT's not a weekend</h1>");
 
-	res.render("list", { kindOfDay: day });
+	res.render("list", {kindOfDay: day, NewListItems: items});
 });
 
+
+app.post('/', (req, res) => {
+	var item = req.body.newItem;
+	//console.log(new_item);
+	items.push(item);
+	res.redirect("/");
+	
+})
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
 })
